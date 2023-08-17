5962d33cc52e55b4a16c412b34d3efd154d84535
Babatunde Adeniran <tuneshman@Babatundes-MacBook-Pro.local>
2023-08-17 22:06:36 +0100

    add auto expand functionality

--- src/components/RenderTree.tsx
+++ src/components/RenderTree.tsx
@@ -4,20 +4,34 @@ import type { TreeProps } from "antd/es/tree";
 import { createDataTree } from "../utils/data";
 import response from "../data/response";
 import { Typography, Grid } from "@mui/material";
-import { DownOutlined } from '@ant-design/icons';
-
+import { DownOutlined } from "@ant-design/icons";
+import { AntTreeNodeProps } from "antd/lib/tree/Tree";
 
 const treeData = createDataTree(response.data.categories, "0");
 
 const RenderTree: React.FC = () => {
   const [checkedKeys, setCheckedKeys] = useState<string[]>([]);
   const [selectedKeys, setSelectedKeys] = useState<string[]>([]);
+  const [expandedKeys, setExpandedKeys] = useState<string[]>([]);
+
   const onSelect: TreeProps["onSelect"] = (selectedKeys) => {
     setSelectedKeys(selectedKeys as string[]);
   };
 
-  const onCheck: TreeProps["onCheck"] = (checkedKeys) => {
+  const onCheck: TreeProps["onCheck"] = (
+    checkedKeys,
+    { node }: AntTreeNodeProps
+  ) => {
+    const parentNodeKey = node!.props.eventKey;
+    if (!expandedKeys.includes(parentNodeKey)) {
+      setExpandedKeys([...expandedKeys, parentNodeKey]);
+    }
+
     setCheckedKeys(checkedKeys as string[]);
+  };
+
+  const onExpand: TreeProps["onExpand"] = (expandedKeys) => {
+    setExpandedKeys(expandedKeys as string[]);
   };
 
   return (
@@ -47,9 +61,11 @@ const RenderTree: React.FC = () => {
         checkable
         onSelect={onSelect}
         onCheck={onCheck}
+        onExpand={onExpand}
         treeData={treeData}
         checkedKeys={checkedKeys}
         selectedKeys={selectedKeys}
+        expandedKeys={expandedKeys}
         switcherIcon={<DownOutlined />}
       />
     </>
